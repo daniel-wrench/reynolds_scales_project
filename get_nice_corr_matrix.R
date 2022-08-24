e4945df8fbc48ea381d233ccf428e7d35205dcd9
Daniel Wrench <daniel@harmonic.co.nz>
2022-08-24 15:20:25 +1200

    pipeline is functional and complete

--- get_nice_corr_matrix.R
+++ get_nice_corr_matrix.R
@@ -1,11 +1,12 @@
 # Get nice, ordered correlation matrix heatmap 
 
-
 library(corrplot)
 library(tidyverse)
+library(lares)
 
 data_raw <- read.csv("~/Research/reynolds_scales_project/data/processed/df_complete.csv")
-data_clean <- data[,3:(ncol(data)-1)]
+data_clean <- data_raw[1:28,2:ncol(data_raw)]
+data_clean <- relocate(data_clean, c(taylor_scale, corr_scale_int, corr_scale_exp_fit, corr_scale_exp_trick))
 corrmatrix <- data_clean %>% cor(use="complete.obs")
 
 # Bad example
@@ -35,3 +36,10 @@ corrplot(corrmatrix,
          title = "Pearson correlation coefficients to 2dp", 
          mar=c(0,0,1,0)
 )
+
+corr_cross(data_clean,
+           #max_pvalue = 0.05, # display only significant correlations (at 5% level)
+           top = 10
+)
+
+corr_var(data_clean, taylor_scale)
