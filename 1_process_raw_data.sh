b178cb2ac5dc6c4859a8b17eba058e1a5a325bf1
Daniel Wrench <daniel@harmonic.co.nz>
2022-09-14 16:39:31 +1200

    adding working bash script to pre-process data

---
+++ 1_process_raw_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+#SBATCH --job-name          1_process_raw_data
+#SBATCH --partition         parallel
+#SBATCH --nodelist          c01n01
+#SBATCH --reservation       spacejam
+#SBATCH --mem-per-cpu       1G
+#SBATCH --cpus-per-task     1
+#SBATCH --time              00:15:00
+#SBATCH --output            %x_%j.out
+#SBATCH --error             %x_%j.err
+
+source venv/Scripts/activate
+
+python process_data_wind_electrons.py
+python process_data_wind_protons.py
+python process_data_omni.py
+python process_data_wind_mfi_hr.py
+python process_data_wind_mfi_lr.py
+
+echo "FINISHED"
