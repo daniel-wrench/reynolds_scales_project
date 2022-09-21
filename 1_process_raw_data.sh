9659698b580adbb0e0d58b3df5093725547f9cfb
Daniel Wrench <spanner199@gmail.com>
2022-09-21 17:20:45 +1200

    adding updated sbatch script, working nicely

--- 1_process_raw_data.sh
+++ 1_process_raw_data.sh
@@ -1,25 +1,26 @@
 #!/bin/bash -e
 
 #SBATCH --job-name          1_process_raw_data
-#SBATCH --partition         parallel
-#SBATCH --nodelist          c01n01
-#SBATCH --reservation       spacejam
+#SBATCH --partition         quicktest
+##SBATCH --nodelist          spj01
 #SBATCH --mem-per-cpu       1G
 #SBATCH --cpus-per-task     1
 #SBATCH --time              00:15:00
 #SBATCH --output            %x_%j.out
 #SBATCH --error             %x_%j.err
+#SBATCH --mail-type	    BEGIN, END, FAIL
+#SBATCH --mail-user	    daniel.wrench@vuw.ac.nz
 
 ## For running locally
-source venv/Scripts/activate
+#source venv/Scripts/activate
 
 ## For running in Raapoi
-#source venv/bin/activate
+source venv/bin/activate
 
-#python process_data_wind_electrons.py
-#python process_data_wind_protons.py
+python process_data_wind_electrons.py
+python process_data_wind_protons.py
 python process_data_omni.py
-# python process_data_wind_mfi_hr.py
-# python process_data_wind_mfi_lr.py
+python process_data_wind_mfi_hr.py
+python process_data_wind_mfi_lr.py
 
 echo "FINISHED"
