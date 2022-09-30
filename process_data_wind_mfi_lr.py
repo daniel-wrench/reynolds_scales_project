0a5f0d6f3e928e3eeea0ad4925a3f050e45cdecd
Daniel Wrench <daniel@harmonic.co.nz>
2022-09-30 17:11:18 +1300

    editing process_raw_data loop to handle exceptions

--- process_data_wind_mfi_lr.py
+++ process_data_wind_mfi_lr.py
@@ -15,10 +15,27 @@ def get_cdf_paths(subfolder):
 file_paths = [get_cdf_paths(subfolder) for subfolder in get_subfolders(
     project_path + 'data/raw/wind/mfi/mfi_h2/')]
 
-df = pd.concat([pd.concat([pipeline(cdf_file_name, varlist=['Epoch', 'BF1', 'BGSE'],
-                                    cadence='5S') for cdf_file_name in sub]) for sub in file_paths]).sort_index()
+# df = pd.concat([pd.concat([pipeline(cdf_file_name, varlist=['Epoch', 'BF1', 'BGSE'],
+#                                     cadence='5S') for cdf_file_name in sub]) for sub in file_paths]).sort_index()
 
-df.to_pickle(project_path + 'data/processed/wi_h2_mfi_lr.pkl')
+df = pd.DataFrame({})
+
+for sub in file_paths:
+    for cdf_file_name in sub:
+        print("Reading " + cdf_file_name) 
+        try:
+            temp_df = pipeline(
+                cdf_file_name, 
+                varlist=['Epoch', 'BF1', 'BGSE'], 
+                cadence='5S'
+            )
+            df = pd.concat([df, temp_df])
+        except:
+            print("Error reading CDF file; moving to next file")
+            nan_df = pd.DataFrame({}) # empty dataframe
+            df = pd.concat([df, nan_df])
+
+#df.to_pickle(project_path + 'data/processed/wi_h2_mfi_lr.pkl')
 
 print("\n\nProcessed Wind MFI low-res data:\n")
 print(df.head())
