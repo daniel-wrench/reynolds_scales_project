406cf128de65b1b402579c5e1296d84fbfb7080f
Daniel Wrench <daniel@harmonic.co.nz>
2022-10-13 08:59:48 +1300

    updating submission script

--- 1_process_raw_data.sh
+++ 1_process_raw_data.sh
@@ -2,9 +2,9 @@
 
 #SBATCH --job-name          1_process_raw_data
 #SBATCH --partition         quicktest
-##SBATCH --nodelist          spj01
-#SBATCH --mem		    2G
-#SBATCH --cpus-per-task     6
+##SBATCH --nodelist         spj01
+#SBATCH --mem-per-cpu		2G
+#SBATCH --cpus-per-task     4
 #SBATCH --time              00:30:00
 #SBATCH --output            %x_%j.out
 #SBATCH --error             %x_%j.err
@@ -18,10 +18,10 @@ source ActivatePython.sh
 echo "JOB STARTED"
 date
 
-mpirun --oversubscribe -n 4 python process_raw_data.py omni_path omni_vars omni_thresh int_size None
-#python process_raw_data.py electron_path    electron_vars   electron_thresh int_size    None
-#python process_raw_data.py proton_path      proton_vars     proton_thresh   int_size    None
-#python process_raw_data.py mag_path         mag_vars        mag_thresh      dt_hr       dt_lr
+mpirun --oversubscribe -n 4 python process_raw_data.py omni_path        omni_vars       omni_thresh     int_size    None
+mpirun --oversubscribe -n 4 python process_raw_data.py electron_path    electron_vars   electron_thresh int_size    None
+mpirun --oversubscribe -n 4 python process_raw_data.py proton_path      proton_vars     proton_thresh   int_size    None
+mpirun --oversubscribe -n 4 python process_raw_data.py mag_path         mag_vars        mag_thresh      dt_hr       dt_lr
 
 ## For once code has been made parallel
 #mpirun -n 6 python process_raw_data.py
