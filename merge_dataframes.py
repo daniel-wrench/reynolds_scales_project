33239a8c0c6f2989c96902736676081355ff3844
Daniel Wrench <daniel@harmonic.co.nz>
2022-10-14 12:09:28 +1300

    as previous

--- merge_dataframes.py
+++ merge_dataframes.py
@@ -1,42 +1,26 @@
 import pandas as pd
 import glob
-import params
 import os
-import sys
 
-# Smaller version of the one in process_raw_data.py
-sys_arg_dict = {
-    # arg1
-    "mag_path": params.mag_path,
-    "omni_path": params.omni_path,
-    "proton_path": params.proton_path,
-    "electron_path": params.electron_path,
+file_paths = sorted(glob.iglob("data/processed/dataset_*.pkl"))
 
-    # arg2
-    "int_size": params.int_size,
-    "dt_hr": params.dt_hr,
-    "dt_lr": params.dt_lr
-}
-
-dir_path = 'data/processed/' + sys_arg_dict[sys.argv[1]] + sys_arg_dict[sys.argv[2]]
-file_paths = sorted(glob.iglob(dir_path + '_*.pkl'))
-
+print("MERGING DATASETS")
 df_merged = pd.DataFrame({})
 for file in file_paths:
+    print("Reading " + file)
     df_merged = pd.concat([df_merged, pd.read_pickle(file)])
     os.remove(file)
 df_merged = df_merged.sort_index()
 
-print("MERGED DATAFRAMES FROM "+ dir_path)
+stats = df_merged.describe()
+
+print("\nSAVING MERGED DATASET AND SUMMARY STATS TABLE\n")
+print(df_merged.info())
+
+df_merged.to_csv("data/processed/db_wind.csv", index=False)
+stats.to_csv("data/processed/db_wind_summary_stats.csv")
+
 print("\nChecking for missing data:")
 print(df_merged.isna().sum()/len(df_merged))
 print("\n")
-print(df_merged.info())
-print("\n")
-print(df_merged.head())
-print("...")
-print(df_merged.tail())
 print("##############################################")
-
-df_merged.to_pickle(dir_path + ".pkl")
-#rm data/processed/wind/3dp/3dp_elm2/12H_*.pkl
