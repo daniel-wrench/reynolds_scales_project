3bd27f99e4034e32ec0094f103b1b32179456f9b
Daniel Wrench <daniel@harmonic.co.nz>
2022-12-02 16:57:39 +1300

    improving plotting, outlier removal

--- 4_analyse_results.py
+++ 4_analyse_results.py
@@ -4,61 +4,79 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import utils
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv("data/processed/db_wind_95_98.csv")
+# TEMP CODE WHEN NEEDING TO MERGE FILES (i.e., did not run on all data at once)
+
+# df_1 = pd.read_csv("data/processed/db_wind_95_98.csv")
 # df_2 = pd.read_csv("data/processed/db_wind_99_07.csv")
 
 # df_1 = df_1.set_index("Timestamp").sort_index()
 # df_2 = df_2.set_index("Timestamp").sort_index()
-# df_merged = pd.concat([df_1, df_2], verify_integrity=True)
-# df_merged.index = pd.to_datetime(df_merged.index)
+# df_omni = df_1[["vsw", "p", "Bomni"]] 
 
-# df_merged["1998-12-30":"1999-01-03"]
+# We have the entire OMNI data in each dataframe
+# We need to exclude it so it doesn't get added together during the following merging process
+# which takes into account the ragged transitions from one df to the next
 
-# BEFORE DOING THE FOLLOWING, EXCLUDE OMNI VARS FROM MERGE AS THESE ARE DOUBLING UP, SUM WILL BE BAD
-# Dealing with some occasional duplication of timestamps due to a timestamp at the end of a file
-# also appearing at the start of the next file 
+# df_merged = pd.concat([df_1, df_2], verify_integrity=False)
+# df_merged = df_merged.drop(["vsw", "p", "Bomni"], axis=1)
+# df_merged.index.has_duplicates
+
+# Can also check for duplicate timestamps during the concatentation with the following: 
+#df_merged = pd.concat([df_1, df_2], verify_integrity=True)
+#ValueError: Indexes have overlapping values
+
 #df_merged = df_merged.groupby(df_merged.index).agg(sum)
 # Dealing with any resultant 0s from summing to NAs together
 #df_merged = df_merged.replace(0, np.nan)
 
-# df_merged_cleaned = df_merged[~df_merged.index.duplicated(keep='last')]
+# df_merged.index = pd.to_datetime(df_merged.index)
+# df_omni.index = pd.to_datetime(df_omni.index)
+# df = utils.join_dataframes_on_timestamp(df_merged, df_omni)
+# df.index.has_duplicates
 
-# df_merged_cleaned["1998-12-30":"1999-01-03"]
+# Checking merge (border between end of first file and start of second, with a ragged transition)
+# df_merged_final["1998-12-30":"1999-01-03"]
 
-# Calculating Reynolds number with three different methods
+# TEMP CODE FOR CALCULATING REYNOLDS NUMBERS 
 
-df["Re_lt"] = (df["tcf"]/df["ttc"])**2
-df["Re_di"] = ((df["tcf"]*df["vsw"])/df["di"])**(4/3)
-df["Re_tb"] = ((df["tcf"])/(1/df["tb"]))**(4/3)
+# df["Re_lt"] = (df["tcf"]/df["ttc"])**2
+# df["Re_di"] = ((df["tcf"]*df["vsw"])/df["di"])**(4/3)
+# df["Re_tb"] = ((df["tcf"])/(1/df["tb"]))**(4/3)
 
-df[["tcf", "ttc", "Re_di", "Re_lt", "Re_tb"]].describe()
-np.mean(df.Re_lt).round(-4)
-np.mean(df.Re_di).round(-4)
-np.mean(df.Re_tb).round(-4)
+# df[["tcf", "ttc", "Re_di", "Re_lt", "Re_tb"]].describe()
+# np.mean(df.Re_lt).round(-4)
+# np.mean(df.Re_di).round(-4)
+# np.mean(df.Re_tb).round(-4)
 
-# Data cleaning
-df.loc[df["Re_lt"] > 1e7, "Re_lt"] = np.nan
-#df.loc[df["Re_di"] > 1e7, "Re_lt"] = np.nan
-
-df[["di", "vsw", "ttu", "Re_di", "Re_lt"]].describe().round()
+#df[["di", "vsw", "ttk", "ttu", "ttc", "Re_di", "Re_lt", "Re_tb"]].describe().round(2)
 # CHECK MAX VALS
 
-df.to_csv("data/processed/db_wind_with_re.csv", index=False)
+#df.to_csv("data/processed/wind_database.csv")
 
 ######################################################
 
-df = pd.read_csv("data/processed/db_wind_with_re.csv")
+df = pd.read_csv("data/processed/wind_database.csv")
+
+#### DATA CLEANING (dealing with outliers)
 
 # Drop points where ratio > 100 and ratio < 0.01 
+# or as below, drop very large values (skewed distributions)
+
+df_cleaned = df[df["ttc"] > 1] 
+df_cleaned.Re_lt.describe()
+
+# Still have outlier values in top 25% - check these
+df_cleaned[["Timestamp", "ttu", "qi", "qk", "ttc", "Re_lt", "Re_di"]][df.Re_lt>1e7]
+
 # Save these extreme cases as case studies (as with strange slopes), but exclude from main statistical analysis
 
-
-### Scatterplots and histograms of Re ###
+### SCATTERPLOTS AND HISTOGRAMS OF RE ###
 
 g = sns.JointGrid(
-    data=df, 
+    data=df_cleaned, 
     x="Re_di", 
     y="Re_lt", 
     xlim = (1e4, 3e6), 
@@ -89,54 +107,52 @@ y0, y1 = g.ax_joint.get_ylim()
 lims = [max(x0, y0), min(x1, y1)]
 g.ax_joint.plot(lims, lims, '-r')
 
+plt.show()
+
 # Draw a regression line
 # (for some reason the following looks weird)
 #g.plot_joint(sns.regplot, scatter=False, ci=False)
 
-#fit = np.polyfit(np.array(df_clean["Re_di"]), np.array(df_clean["Re_lt"]), deg=1)
-#g.ax_joint.plot(df_clean[["Re_di"]], df_clean["Re_di"]*fit[0]+fit[1], '-b')
+#fit = np.polyfit(np.array(df_cleaned_clean["Re_di"]), np.array(df_cleaned_clean["Re_lt"]), deg=1)
+#g.ax_joint.plot(df_cleaned_clean[["Re_di"]], df_cleaned_clean["Re_di"]*fit[0]+fit[1], '-b')
 
-# df_clean = df.dropna()
-# reg = LinearRegression().fit(df_clean[["Re_di"]], df_clean[["Re_lt"]])
-# Re_lt_predict = reg.predict(df_clean[["Re_di"]])
-
-plt.show()
+# df_cleaned_clean = df_cleaned.dropna()
+# reg = LinearRegression().fit(df_cleaned_clean[["Re_di"]], df_cleaned_clean[["Re_lt"]])
+# Re_lt_predict = reg.predict(df_cleaned_clean[["Re_di"]])
 
 # The coefficients
 #print("Coefficients: \n", reg.coef_)
 #print(reg.intercept_)
 
 #from sklearn.metrics import mean_squared_error, r2_score
-#print("Mean squared error: %.2f" % mean_squared_error(df_clean[["Re_lt"]], Re_lt_predict))
+#print("Mean squared error: %.2f" % mean_squared_error(df_cleaned_clean[["Re_lt"]], Re_lt_predict))
 # The coefficient of determination: 1 is perfect prediction
-#print("Coefficient of determination: %.2f" % r2_score(df_clean[["Re_lt"]], Re_lt_predict))
+#print("Coefficient of determination: %.2f" % r2_score(df_cleaned_clean[["Re_lt"]], Re_lt_predict))
 
 
+### PLOTS FOR ALL THREE RE APPROXIMATIONS
 
-
-### Same but for all 3 Re methods
-
-df_re = df[["Re_di", "Re_tb", "Re_lt"]]
-df_re = df_re.dropna()
-f = sns.PairGrid(df_re, diag_sharey=False, corner=False)
+df_cleaned_re = df_cleaned[["Re_di", "Re_tb", "Re_lt"]]
+df_cleaned_re = df_cleaned_re.dropna()
+f = sns.PairGrid(df_cleaned_re, diag_sharey=False, corner=False)
 f.map_lower(sns.histplot, log_scale=True)
 #f.map_lower(sns.regplot, scatter=False)
 f.map_diag(sns.kdeplot)
 plt.show()
 
-### Time series of Re ###
+### TIME SERIES OF RE
 
-df.Timestamp = pd.to_datetime(df.Timestamp)
-df_1995 = df[(df.Timestamp > "1997-01-01") & (df.Timestamp < "1998-01-01")]
+df_cleaned.Timestamp = pd.to_datetime(df_cleaned.Timestamp)
+df_cleaned_1995 = df_cleaned[(df_cleaned.Timestamp > "1997-01-01") & (df_cleaned.Timestamp < "1998-01-01")]
 
 # Min-max normalisation
-df_1995["Re_di_norm"] = (df_1995["Re_di"]-df_1995["Re_di"].min())/(df_1995["Re_di"].max()-df_1995["Re_di"].min())
-df_1995["Re_lt_norm"] = (df_1995["Re_lt"]-df_1995["Re_lt"].min())/(df_1995["Re_lt"].max()-df_1995["Re_lt"].min())
-df_1995["Re_tb_norm"] = (df_1995["Re_tb"]-df_1995["Re_tb"].min())/(df_1995["Re_tb"].max()-df_1995["Re_tb"].min())
+df_cleaned_1995["Re_di_norm"] = (df_cleaned_1995["Re_di"]-df_cleaned_1995["Re_di"].min())/(df_cleaned_1995["Re_di"].max()-df_cleaned_1995["Re_di"].min())
+df_cleaned_1995["Re_lt_norm"] = (df_cleaned_1995["Re_lt"]-df_cleaned_1995["Re_lt"].min())/(df_cleaned_1995["Re_lt"].max()-df_cleaned_1995["Re_lt"].min())
+df_cleaned_1995["Re_tb_norm"] = (df_cleaned_1995["Re_tb"]-df_cleaned_1995["Re_tb"].min())/(df_cleaned_1995["Re_tb"].max()-df_cleaned_1995["Re_tb"].min())
 
-sns.lineplot(data=df_1995, x="Timestamp", y="Re_di_norm", label="Re_di")
-sns.lineplot(data=df_1995, x="Timestamp", y="Re_lt_norm", label="Re_lt")
-sns.lineplot(data=df_1995, x="Timestamp", y="Re_tb_norm", label="Re_tb")
+sns.lineplot(data=df_cleaned_1995, x="Timestamp", y="Re_di_norm", label="Re_di")
+sns.lineplot(data=df_cleaned_1995, x="Timestamp", y="Re_lt_norm", label="Re_lt")
+sns.lineplot(data=df_cleaned_1995, x="Timestamp", y="Re_tb_norm", label="Re_tb")
 
 plt.semilogy()
 plt.show()
